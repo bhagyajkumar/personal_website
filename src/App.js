a01d9cc794e19ccf01bd53308613321a6e910ada
bhagyajkumar <bhagyajkumar1@gmail.com>
2021-11-13 08:11:22 +0530

    done hash routing for github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import "./app.css"
 import Home from "./pages/Home";
